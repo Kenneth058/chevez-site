14a4fe04f2a4bfa4c60fccf99eac5a17dd10db07
KennethChevezTN <kenneth.chevez@truenorth.co>
2021-09-19 23:40:59 -0600

    Test with snapshots

--- pages/__tests__/index.test.js
+++ pages/__tests__/index.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import renderer from 'react-test-renderer'
 import Index from '../index'
 
 test('Should render index page', () => {
   render(<Index />)
-  expect(screen.getByText('Hola mundo a mi canal')).toBeInTheDocument()
+  expect(screen.getByTestId('test-1')).toBeInTheDocument()
+})
+
+test('Should matches snapshot', () => {
+  const tree = renderer.create(<Index />)
+  expect(tree).toMatchSnapshot()
 })
